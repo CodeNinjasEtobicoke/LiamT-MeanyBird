140c90d7564800911ac2c1d69077e980bf8ea31e
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-04-11 15:37:56 -0400

    A new repo
    Liam! Don't forget to push your project into github...

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    //Game manager object
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Default Velocity")]
+    public float velocity = 1;
+    //physics for the bird
+    private Rigidbody2D rb;
+    //height of the bird object on the y axis
+    private float objectiveHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
