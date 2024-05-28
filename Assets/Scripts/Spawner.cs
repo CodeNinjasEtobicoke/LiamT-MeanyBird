9fdbf11036b83e063f91fa28be913e8d1ae780e0
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-05-28 16:36:56 -0400

    meanybird

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
+
 {
-    [Header("Spikes Object for controlling the game")]
     // Start is called before the first frame update
+    
+    
+    [Header("Spikes Object for controlling the game")]
+     public GameObject spikes;
+    [Header("Default Height")]
+        public float height; 
     void Start()
     {
-
-
+        InvokeRepeating("InstantiateObject", 1f, 4f);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
+
 
     }
 }
+
+
