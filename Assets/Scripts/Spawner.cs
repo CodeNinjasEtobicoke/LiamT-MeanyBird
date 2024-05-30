63c897472968d8e1c1fcaca6de3126f73295fb20
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2024-05-30 18:12:47 -0400

    U LEFT OFF ON STEP 26

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
-
 {
     // Start is called before the first frame update
-    
-    
     [Header("Spikes Object for controlling the game")]
-     public GameObject spikes;
+    public GameObject spikes;
     [Header("Default Height")]
-        public float height; 
+    public float height; 
+
+
     void Start()
     {
         InvokeRepeating("InstantiateObject", 1f, 4f);
@@ -22,8 +21,11 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(5, Random.Range(-height, height), 0);
+    }
 
-
+    void InstantiateObject()
+    {
+        Instantiate(spikes, transform.position, transform.rotation);
     }
 }
 
