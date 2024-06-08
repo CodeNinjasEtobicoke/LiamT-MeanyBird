2c393bcd9f2813847c13bcf41b534b0cb9911118
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2024-06-08 13:26:20 -0400

    DONE~~

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     // Start is called before the first frame update
-    [Header("Spikes Object for controlling the game")]
+    [Header("spikes Object for controlling the game")]
     public GameObject spikes;
     [Header("Default Height")]
     public float height; 
